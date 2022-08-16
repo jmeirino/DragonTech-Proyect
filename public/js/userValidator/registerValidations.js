d18e3a25b1ae15b6cabdf2ec5e9b8e7ef0dacacd
jmeirino <juanchomeirino@gmail.com>
2022-08-16 09:17:48 -0300

    validaciones front ok

--- public/js/userValidator/registerValidations.js
+++ public/js/userValidator/registerValidations.js
@@ -7,8 +7,8 @@ function validarFormulario(event) {
     event.preventDefault();
 
     const campoNombre = document.querySelector('input.nombre');
-    const campoApellido = document.querySelector('input#apellido');
-    const campoFechaNacimiento = document.querySelector('input.fechaNacimiento');
+    const campoApellido = document.querySelector('input.apellido');
+    //const campoFechaNacimiento = document.querySelector('input.fechaNacimiento');
     const campoTelefono = document.querySelector('input.telefono');
     const campoEmail = document.querySelector('input.email');
     const campoPassword = document.querySelector('input.password');
@@ -19,6 +19,7 @@ function validarFormulario(event) {
     campoEmail.classList.remove('error');
     campoNombre.classList.remove('error');
     campoApellido.classList.remove('error');
+    //campoFechaNacimiento.classList.remove('error');
     campoTelefono.classList.remove('error');
     campoPassword.classList.remove('error');
     
@@ -34,6 +35,10 @@ function validarFormulario(event) {
         error = true;
         campoApellido.classList.add('error');
     }
+    //if(!validateFecha(campoFechaNacimiento.value.trim())){
+        //error = true;
+        //campoFechaNacimiento.classList.add('error');
+    //}
     if (campoTelefono.value.trim().length < 8){
         error = true;
         campoTelefono.classList.add('error');
@@ -62,3 +67,30 @@ function validateEmail(email) {
     alert("Introduzca una dirección de mail válida")
     return false;
 }
+
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth() + 1; //January is 0!
+var yyyy = today.getFullYear();
+
+if (dd < 10) {
+   dd = '0' + dd;
+}
+
+if (mm < 10) {
+   mm = '0' + mm;
+} 
+    
+today = yyyy + '-' + mm + '-' + dd;
+document.getElementById("datefield").setAttribute("max", today);
+
+
+
+//funcion validar fecha
+//function validateFecha(fechaNacimiento) {
+    //if (fechaNacimiento && fechaNacimiento.match && /^\d{1,2}\/\d{1,2}\/\d{2,4}$/.test(fechaNacimiento)){
+        //return true;
+    //} else {
+        //return false;
+    //}
+//}
